afa8af86022cfd3a7fd26c91859c26e97952d08e
Azeem Mohammed <azeemsm@gmail.com>
2021-03-02 12:00:31 -0500

    make calls with python

--- echo_video_call/make_call.py
+++ echo_video_call/make_call.py
@@ -2,5 +2,5 @@ import requests
 
 f=open("maker.key","r")
 secret_key=f.read()
-requests.post("https://maker.ifttt.com/trigger/YourEventName/with/key"+secret_key)    
+requests.post("https://maker.ifttt.com/trigger/robot_emergency_call/with/key"+secret_key)    
 f.close()
