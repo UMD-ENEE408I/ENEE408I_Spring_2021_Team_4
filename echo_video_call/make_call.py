a8995642a7b6a7bd7b72ace065936a103cc5d6c7
Azeem Mohammed <azeemsm@gmail.com>
2021-03-02 13:19:42 -0500

    fixed typo

--- echo_video_call/make_call.py
+++ echo_video_call/make_call.py
@@ -1,6 +1,7 @@
 import requests
 
 f=open("maker.key","r")
-secret_key=f.read()
-requests.post("https://maker.ifttt.com/trigger/robot_emergency_call/with/key"+secret_key)    
+secret_key=f.read().strip()
+r=requests.post("https://maker.ifttt.com/trigger/robot_emergency_call/with/key/"+secret_key)    
+print(r.text)
 f.close()
