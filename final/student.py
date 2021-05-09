c1198c609230804883d926b4c464f80ff3119d7a
Azeem Mohammed <azeemsm@gmail.com>
2021-05-09 16:26:19 -0400

    student

--- final/student.py
+++ final/student.py
@@ -9,8 +9,20 @@ engine = pyttsx3.init()
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate-40)
 
+'''
 engine.say('This is a test')
 engine.say('The current yoga pose is...')
 engine.say('The quick brown fox jumped over the lazy dog.')
 engine.runAndWait()
+'''
+
+def test_callback(message_dict):
+    global engine
+    if message_dict['type'] == 'pose':
+        print('Value of field pose: {}'.format(message_dict['pose']))
+        engine.say(message_dict['pose'])
+        engine.runAndWait()
+
+def create_client(address):
+    client=RobotChatClient(address, callback=test_callback)
 
