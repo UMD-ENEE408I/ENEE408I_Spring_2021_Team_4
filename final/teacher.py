aa7d251dcc8af6ab49ad72a70a763df66102ade0
Azeem Mohammed <azeemsm@gmail.com>
2021-05-09 16:18:44 -0400

    teacher

--- final/teacher.py
+++ final/teacher.py
@@ -1,16 +1,26 @@
 from robot_chat_client import RobotChatClient
+from robot_chat_server import start_server_func
+from pose.pose_rec_obj import PoseRecognition
 
-
+client=""
 def test_callback(message_dict):
-   
     if message_dict['type'] == 'pose':
         print('Value of field pose: {}'.format(message_dict['pose']))
 
    
+def send_message(message):
+    global client
+    client.send({'type':'pose',
+        'pose':message})
+
 # Run this script directly to invoke this test sequence
 if __name__ == '__main__':
+    start_server_func()
     print('Creating RobotChatClient object')
-    client = RobotChatClient('', callback=test_callback) #ngrok server
+    global client
+    client = RobotChatClient('ws://localhost:5001', callback=test_callback) #ngrok server
+    pr=PoseRecognition(callback=send_message)
+
 
     
 
